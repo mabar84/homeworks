25b3bb8dbc0b31ddb6818e6a83e47babadea5dd8
mabar84 <mabar@tut.by>
2023-12-25 13:35:45 +0300

    hw7 logic is added

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,33 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name < b.name ? 1 : -1)
+                : [...state].sort((a, b) => a.name > b.name ? 1 : -1)
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
     }
 }
+
+// export const checkAC = (): ActionType => {
+//     return {
+//         type: 'check',
+//         payload: 18
+//     }
+// }
+//
+// export const sortAC = (by: 'up' | 'down'): ActionType => {
+//     return {
+//         type: 'sort',
+//         payload: by
+//     }
+// }
