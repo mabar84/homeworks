b22aa4993ba6385dc8c1fa46fbfe3d057ae61f68
mabar84 <mabar@tut.by>
2024-01-10 21:37:58 +0300

    hw11 is done

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -13,12 +13,18 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState('hw11-value2', [0, 100]))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
+        if (typeof value === 'number') {
+            setValue1(value)
+            setValue2([value, value2[1]])
+        } else {
+            setValue1(value[0])
+            setValue2(value)
+        }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
-
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
@@ -29,18 +35,25 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            min={0}
+                            max={100}
+                            value={value1}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={change}
                         />
+                        <span style={{color: 'transparent'}} className={s.number}>{value2[1]}</span>
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            min={0}
+                            max={100}
+                            value={value2}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={change}
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.number}>{value2[1]}</span>
                     </div>
                 </div>
             </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,8 +4,30 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+                marginBottom: 0, // Изменение отступа
+                '& .MuiSlider-thumb': {
+                    width: '18px',
+                    height: '18px',
+                    border: '1px solid #0c2', // Бордер для ползунка
+                    backgroundColor: 'white',
+                },
+                '& .MuiSlider-thumb:before': {
+                    width: '6px',
+                    height: '6px',
+                    backgroundColor: '#0c2',
+                    boxShadow: 'none'
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                    borderRadius: '10px',
+                    border: '1px solid #8B8B8B', // Бордер для шкалы ползунка
+                },
+                '& .MuiSlider-track': {
+                    // display: 'white', // Скрыть трек ползунка
+                    backgroundColor: '#0c2',
+                    border: '1px solid #0c2', // Бордер для шкалы ползунка
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
